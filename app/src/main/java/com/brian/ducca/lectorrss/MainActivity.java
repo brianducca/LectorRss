225529dce3278c88a3a51c694ce14494077c4a31
unknown <alumno@LAB02PC14.labs.extension>
2015-10-08 21:53:06 -0300

    se agrega la lic gnu y un log de prueba

--- app/src/main/java/com/brian/ducca/lectorrss/MainActivity.java
+++ app/src/main/java/com/brian/ducca/lectorrss/MainActivity.java
@@ -1,3 +1,20 @@
+/**
+ *
+ Copyright (C) <2015>  <Brian Ducca>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.brian.ducca.lectorrss;
 
 import android.os.Handler;
@@ -68,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback,
         {
             List<Noticia> milista= (ArrayList)msg.obj;
             MyAdapter a = new MyAdapter(milista,h);
-
+            Log.d("pruebagit","entre");
             /*
             for (Noticia n:milista)
             {
